26fb37c3bd497dd388cfe9bdae180092987d2411
Helen Rong <helen.rong@nav.no>
2026-01-20 13:07:15 +0000

    Endringen på databank

--- models/BT_statistikk_bank/dim_bt_kjonn.sql
+++ models/BT_statistikk_bank/dim_bt_kjonn.sql
@@ -4,10 +4,19 @@
     )
 }}
 
-select distinct kjonn_kode, kjonn_flertall_besk as kjonn_besk, kjonn_nr
-from {{ source('bt_statistikk_bank_dt_kodeverk', 'dim_kjonn') }}
+select
+    distinct kjonn_kode, kjonn_flertall_besk as kjonn_besk, kjonn_nr
+   ,1 as kjonn_nivaa
+   ,case when kjonn_flertall_besk = 'Menn' then 1
+         when kjonn_flertall_besk = 'Kvinner' then 2
+    end sortering
 where kjonn_flertall_besk in ('Kvinner','Menn')
 
 union all
-select 'ALT' as kjonn_kode, 'I alt' as kjonn_besk, -1 as kjonn_nr
+select
+    'ALT' as kjonn_kode, 'I alt' as kjonn_besk, -1 as kjonn_nr
+   ,2 as kjonn_nivaa
+   ,0 as sortering
 from dual
+
+order by kjonn_nr
